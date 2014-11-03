8166063342faf5ddaacdf8dd7c189af58774d3a2
antoine-tran <tranatuan24@gmail.com>
2014-11-03 15:51:00 +0100

    convert hadoop jar to local java command

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HEAP_MEM=-Xmx4096m
-LOG4J="-D log4j.configuration=file:etc/log4j.properties"
+LOG4J=-Dlog4j.configuration=file:etc/log4j.properties
 LIB=./lib
 LOCAL_LIB=./ivy/local-repo
 MAIN_CLASS=$1
@@ -23,6 +23,7 @@ CLPA=$CLPA:$HADOOP_CLASSPATH
 # Add Hadoop conf to classpath
 CLPD=$CLPA:/opt/cloudera/parcels/CDH/lib/hadoop-0.20-mapreduce/conf
 export HADOOP_CLASSPATH="$CLPD:$HADOOP_CLASSPATH"
+echo $HADOOP_CLASSPATH
 echo Starting....
 
 export HADOOP_CLIENT_OPTS="-Xmx2048m $HADOOP_CLIENT_OPTS"
@@ -42,4 +43,4 @@ if [ ! -n "$TERRIER_ETC" ]
 then
   TERRIER_ETC=$TERRIER_HOME/etc
 fi
-hadoop jar bin/streamcorpus-indexer-0.1-SNAPSHOT.jar $MAIN_CLASS -libjars ${HCLPD} $LOG4J -D mapreduce.map.memory.mb=4096 -D mapreduce.map.java.opts=$HEAP_MEM -D mapred.output.compress=true -D mapred.output.compression.type=BLOCK -D mapred.output.compression.codec=org.apache.hadoop.io.compress.BZip2Codec -D mapred.compress.map.output -D mapred.map.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec -D mapred.task.timeout=1200000 -D terrier.home="$TERRIER_HOME" $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+java -Dterrier.home="$TERRIER_HOME" $HEAP_MEM $LOG4J -cp $CLPD $MAIN_CLASS $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
