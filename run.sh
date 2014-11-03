5a3fd06853b0aec29089b094928db709a459aaa7
antoine-tran <tranatuan24@gmail.com>
2014-11-03 15:46:03 +0100

    test using hadoop jar to run Terrier

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HEAP_MEM=-Xmx4096m
-LOG4J=-Dlog4j.configuration=file:etc/log4j.properties
+LOG4J="-D log4j.configuration=file:etc/log4j.properties"
 LIB=./lib
 LOCAL_LIB=./ivy/local-repo
 MAIN_CLASS=$1
@@ -42,4 +42,4 @@ if [ ! -n "$TERRIER_ETC" ]
 then
   TERRIER_ETC=$TERRIER_HOME/etc
 fi
-java -Dterrier.home="$TERRIER_HOME" $HEAP_MEM $LOG4J -cp $CLPD $MAIN_CLASS $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+hadoop jar bin/streamcorpus-indexer-0.1-SNAPSHOT.jar $MAIN_CLASS -libjars ${HCLPD} $LOG4J -D mapreduce.map.memory.mb=4096 -D mapreduce.map.java.opts=$HEAP_MEM -D mapred.output.compress=true -D mapred.output.compression.type=BLOCK -D mapred.output.compression.codec=org.apache.hadoop.io.compress.BZip2Codec -D mapred.compress.map.output -D mapred.map.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec -D mapred.task.timeout=1200000 -D terrier.home="$TERRIER_HOME" $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
