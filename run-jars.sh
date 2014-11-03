475dccc99f45b5ef9e4b50f6024daea495b26fe3
antoine-tran <tranatuan24@gmail.com>
2014-11-03 17:51:38 +0100

    add system properties

--- run-jars.sh
+++ run-jars.sh
@@ -6,6 +6,8 @@
 LIB=$(pwd)/lib
 LOCAL_LIB=$(pwd)/lib
 
+LOG4J=-Dlog4j.configuration=file:etc/log4j.properties
+
 # path to your jar file here. Default is the maven artifact in your local repo
 export JAR_PATH="bin/streamcorpus-indexer-0.1-SNAPSHOT.jar"
 
@@ -45,4 +47,4 @@ then
   TERRIER_ETC=$TERRIER_HOME/etc
 fi
 
-hadoop jar $1 $2 -libjars ${HCLPDA} -D terrier.home="$TERRIER_HOME" -D mapred.output.compress=true -D mapred.output.compression.type=BLOCK -D mapred.output.compression.codec=org.apache.hadoop.io.compress.BZip2Codec -D mapred.compress.map.output -D mapred.map.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec -D mapred.task.timeout=1200000 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+hadoop jar $1 $2 -libjars ${HCLPDA} -Dterrier.home="$TERRIER_HOME" $LOG4J -D mapred.output.compress=true -D mapred.output.compression.type=BLOCK -D mapred.output.compression.codec=org.apache.hadoop.io.compress.BZip2Codec -D mapred.compress.map.output -D mapred.map.output.compression.codec=org.apache.hadoop.io.compress.SnappyCodec -D mapred.task.timeout=1200000 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
