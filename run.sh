ab3dfba58411940cbc274b926426116c05a50baa
antoine-tran <tranatuan24@gmail.com>
2014-11-03 11:58:47 +0100

    add running script

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+HEAP_MEM=-Xmx4096m
+LOG4J=-Dlog4j.configuration=file:etc/log4j.properties
+LIB=./lib
+MAIN_CLASS=$1
+for jarf in $LIB/*.jar
+do
+CLPA=$CLPA:$jarf
+done
+CLPA=$CLPA:bin/streamcorpus-indexer-0.1-SNAPSHOT.jar
+echo Starting....
+
+#setup TERRIER_HOME
+if [ ! -n "$TERRIER_HOME" ]
+then
+  #find out where this script is running
+  TEMPVAR=`dirname $0`
+  #make the path abolute
+  fullPath TEMPVAR
+  #terrier folder is folder above
+  TERRIER_HOME=`dirname $TEMPVAR`
+  echo "Setting TERRIER_HOME to $TERRIER_HOME"
+fi
+#setup TERRIER_ETC
+if [ ! -n "$TERRIER_ETC" ]
+then
+  TERRIER_ETC=$TERRIER_HOME/etc
+fi
+java $HEAP_MEM $LOG4J -cp $CLPA $MAIN_CLASS $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
