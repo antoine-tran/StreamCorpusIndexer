66defa60f8c9213f8150ee68024ab4775b4b46af
antoine-tran <tranatuan24@gmail.com>
2014-11-03 13:19:00 +0100

    fix typo

--- run.sh
+++ run.sh
@@ -31,5 +31,4 @@ if [ ! -n "$TERRIER_ETC" ]
 then
   TERRIER_ETC=$TERRIER_HOME/etc
 fi
-export terrier.home=$TERRIER_HOME
-java $HEAP_MEM $LOG4J -cp $CLPA -Dterrier.home=$TERRIER_HOME $MAIN_CLASS $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
+java -Dterrier.home="$TERRIER_HOME" $HEAP_MEM $LOG4J -cp $CLPA $MAIN_CLASS $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13} ${14} ${15} ${16} ${17} ${18} ${19} ${20}
