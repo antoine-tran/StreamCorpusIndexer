737ec27b3a59f0cf65e69d61747f522da94dadd2
antoine-tran <tranatuan24@gmail.com>
2014-11-06 22:18:28 +0100

    add debugging code2

--- src/de/l3s/streamcorpus/StreamCorpusIndexing.java
+++ src/de/l3s/streamcorpus/StreamCorpusIndexing.java
@@ -225,7 +225,7 @@ public class StreamCorpusIndexing extends Configured implements Tool
 		{
 			if (line.startsWith("#"))
 				continue;
-			System.out.println(lien);
+			System.out.println(line);
 			paths.add(new Path(line));
 		}
 		specBR.close();
