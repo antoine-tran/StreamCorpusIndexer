e00cc3a6ad8f026bae577d2ec44b35de545866af
antoine-tran <tranatuan24@gmail.com>
2014-11-08 10:48:07 +0100

    remove debugging log

--- src/de/l3s/streamcorpus/StreamCorpusIndexing.java
+++ src/de/l3s/streamcorpus/StreamCorpusIndexing.java
@@ -225,7 +225,6 @@ public class StreamCorpusIndexing extends Configured implements Tool
 		{
 			if (line.startsWith("#"))
 				continue;
-			System.out.println(line);
 			paths.add(new Path(line));
 		}
 		specBR.close();
